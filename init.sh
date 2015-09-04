6d77a02758eccaf933b9b5fa6661564de6d67b8a
sonir <isana@sonir.jp>
2015-09-04 20:43:30 +0900

    ver1.0

---
+++ init.sh
@@ -0,0 +1,6 @@
+echo "# 1509_skl_sketches" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sonir/1509_skl_sketches.git
+git push -u origin master
